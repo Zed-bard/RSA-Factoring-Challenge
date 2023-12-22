ef23cdf2fd2e799ac3e975cb1ea014d6d925d5e3
Zed-bard <zerihunbard@gmail.com>
2023-12-22 09:59:54 -0800

    updated

--- factors.c
+++ factors.c
@@ -1,30 +1,59 @@
-#include "factor.h"
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
 /**
- * factorize - The function factorize a number
- * @buffer: pointer to the address of the number
+ * read_textfile- read and sent to stdout a number of charss.
  *
- * Author: Thaoban Abdrasheed
- * Return: int
+ *@filename: name of file
+ *@letters: number of letters
+ * Return: Number of printed chars.
  */
-int factorize(char *buffer)
+int main(int ac, char **av)
 {
+	int i, j, flag, fd, h;
+	int leido = 0;
+	long int number, number2, k;
+	char *buf, *buf2;
+	int letters = 100;
 
-	u_int32_t num;
-	u_int32_t i;
-
-	num = atoi(buffer);
-
-
-	for (i = 2; i < num; i++)
+	leido = leido;
+	ac = ac;
+	number = 0;
+	fd = open(av[1], O_RDONLY);
+	buf = malloc(1000 * sizeof(char));
+	buf2 = malloc(120 * sizeof(char));
+	leido = read(fd, buf, letters);
+ 	i = 0;
+	while (buf[i] != 0)
 	{
-		if (num % i == 0)
+		j = 0;
+		for (h = 0; h <= 100; h++)
+			buf2[h] = 0;
+		while (buf[i] != 10)
+			 {
+				 buf2[j] = buf[i];
+				 i++;
+				 j++;
+			 }
+		number = atol(buf2);
+		flag = 0;
+		for (k = 2; k < number /2 && flag == 0; k++)
 		{
-			printf("%d=%d*%d\n",num,num/i,i);
-			break;
+			if (number % k == 0)
+				flag = 1;
 		}
+		number2 = number / (k - 1);
+		printf("%li=%li*%li\n", number, number2, k - 1);
+		i++;
 	}
-
-return (0);
+	free(buf);
+	free(buf2);
+	close(fd);
+	return(0);
 }
 
