4f505ab66f41484c8a6c2fbae239828c174678a3
Zed-bard <zerihunbard@gmail.com>
2023-12-24 05:43:57 -0800

    factors.py

--- factors.py
+++ factors.py
@@ -1,23 +1,42 @@
 #!/usr/bin/python3
 #import pdb; pdb.set_trace()
+
 import math
 import sys
 
-filename = sys.argv[1]
+def factorize(number):
+    factors = []
+    divisor = 2
 
-with open(filename, 'r', encoding='utf-8') as f:
-    a = f.read()
-    a_l = a.count("\n")
-lineas = 0
-b = 0
-with open(filename, 'r', encoding='utf-8') as f:
-    while lineas < a_l:
-        n1 = int (f.readline())
-        lineas += 1
-        a = 2
-        while a < n1//2:
-            if math.fmod(n1, a) == 0:
-                print ("{:d} = {:d} * {:d}".format(n1, a, int(n1/a), end=""))
-                a = n1
-            a += 1
+    while divisor <= math.isqrt(number):
+        if number % divisor == 0:
+            factors.append(divisor)
+            number //= divisor
+        else:
+            divisor += 1
 
+    if number > 1:
+        factors.append(number)
+
+    return factors
+
+def main(file_path):
+    with open(file_path, 'r') as file:
+        numbers = [int(line.strip()) for line in file]
+
+    for number in numbers:
+        factors = factorize(number)
+        print(f"{number}={'*'.join(map(str, factors))}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: factors <file>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    
+    try:
+        main(file_path)
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
