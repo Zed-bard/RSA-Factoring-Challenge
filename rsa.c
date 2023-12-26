add189d44c54e2d049d3c406dc5d4a09acafdb41
Zed-bard <zerihunbard@gmail.com>
2023-12-26 00:22:04 -0800

    modification

--- rsa.c
+++ rsa.c
@@ -1,59 +1,55 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
-/**
- * read_textfile- read and sent to stdout a number of charss.
- *
- *@filename: name of file
- *@letters: number of letters
- * Return: Number of printed chars.
- */
-int main(int ac, char **av)
-{
-	int i, j, flag, fd, h;
-	int leido = 0;
-	long int number, number2, k;
-	char *buf, *buf2;
-	int letters = 100;
 
-	leido = leido;
-	ac = ac;
-	number = 0;
-	fd = open(av[1], O_RDONLY);
-	buf = malloc(1000 * sizeof(char));
-	buf2 = malloc(120 * sizeof(char));
-	leido = read(fd, buf, letters);
- 	i = 0;
-	while (buf[i] != 0)
-	{
-		j = 0;
-		for (h = 0; h <= 100; h++)
-			buf2[h] = 0;
-		while (buf[i] != 10)
-			 {
-				 buf2[j] = buf[i];
-				 i++;
-				 j++;
-			 }
-		number = atol(buf2);
-		flag = 0;
-		for (k = 2; k < number /2 && flag == 0; k++)
-		{
-			if (number % k == 0)
-				flag = 1;
-		}
-		number2 = number / (k - 1);
-		printf("%li=%li*%li\n", number, number2, k - 1);
-		i++;
-	}
-	free(buf);
-	free(buf2);
-	close(fd);
-	return(0);
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    
+    return 1;
 }
 
+void findPrimeFactors(int num) {
+    printf("%d=", num);
+    
+    for (int i = 2; i <= num; i++) {
+        if (isPrime(i) && num % i == 0) {
+            printf("%d", i);
+            num /= i;
+            
+            if (num != 1) {
+                printf("*");
+            }
+            
+            i--;
+        }
+    }
+    
+    printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: ./rsa <input_file>\n");
+        return 1;
+    }
+    
+    FILE *inputFile = fopen(argv[1], "r");
+    if (inputFile == NULL) {
+        printf("Error opening input file.\n");
+        return 1;
+    }
+    
+    int num;
+    while (fscanf(inputFile, "%d", &num) == 1) {
+        findPrimeFactors(num);
+    }
+    
+    fclose(inputFile);
+    return 0;
+}
