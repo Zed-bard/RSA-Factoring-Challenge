345c7f7298c91c8c1a7022d77a6cc347f47e7fac
Zed-bard <zerihunbard@gmail.com>
2023-12-26 00:42:33 -0800

    modification

--- factors.c
+++ factors.c
@@ -1,58 +1,48 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-#include <string.h>
-/**
- * read_textfile- read and sent to stdout a number of charss.
- *
- *@filename: name of file
- *@letters: number of letters
- * Return: Number of printed chars.
- */
-int main(int ac, char **av)
-{
-	int i, j, flag, fd, h;
-	int leido = 0;
-	long int number, number2, k;
-	char *buf, *buf2;
-	int letters = 100;
 
-	leido = leido;
-	ac = ac;
-	number = 0;
-	fd = open(av[1], O_RDONLY);
-	buf = malloc(1000 * sizeof(char));
-	buf2 = malloc(120 * sizeof(char));
-	leido = read(fd, buf, letters);
- 	i = 0;
-	while (buf[i] != 0)
-	{
-		j = 0;
-		for (h = 0; h <= 100; h++)
-			buf2[h] = 0;
-		while (buf[i] != 10)
-			 {
-				 buf2[j] = buf[i];
-				 i++;
-				 j++;
-			 }
-		number = atol(buf2);
-		flag = 0;
-		for (k = 2; k < number /2 && flag == 0; k++)
-		{
-			if (number % k == 0)
-				flag = 1;
-		}
-		number2 = number / (k - 1);
-		printf("%li=%li*%li\n", number, number2, k - 1);
-		i++;
-	}
-	free(buf);
-	free(buf2);
-	close(fd);
-	return(0);
+void factorize(long int number) {
+    printf("%li=", number);
+
+    int factor = 2;
+    while (factor <= sqrt(number)) {
+        if (number % factor == 0) {
+            printf("%i*", factor);
+            number = number / factor;
+        } else {
+            factor++;
+        }
+    }
+    printf("%li\n", number);
+}
+
+int main(int argc, char *argv[]) {
+    FILE *file;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    if (argc != 2) {
+        printf("Usage: factors <file>\n");
+        return 1;
+    }
+
+    file = fopen(argv[1], "r");
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    while ((read = getline(&line, &len, file)) != -1) {
+        long int number = atol(line);
+        factorize(number);
+    }
+
+    fclose(file);
+    if (line) {
+        free(line);
+    }
+
+    return 0;
 }
